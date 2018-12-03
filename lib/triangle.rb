c2db58d4b7b191ba68093941f11044469545eadf
Akili Humphrey <akilihumphrey@gmail.com>
2018-12-03 23:56:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,17 @@
 class Triangle
-  # write code here
+ 
+  def initialize(side1, side2, side3)
+    @kind = kind
+  end
+ 
+  def kind
+    self.partner = person
+    
+    if person.class != Person
+        raise TriangleError
+      end
+ 
+  class TriangleError < StandardError
+  end
+end
 end
